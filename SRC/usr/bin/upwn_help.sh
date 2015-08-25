16edd0e97d340aee77c6a520dbb66c4365d5ecc7
rhcp <rhcp011235@me.com>
2015-08-25 12:43:48 -0400

    Update to Upwn 1.4.2 - Bug Free - Add support for iPhone 5,3 and iPhone 5,4 on IOS8.4 (iPhone 5,2 coming soon)

---
+++ SRC/usr/bin/upwn_help.sh
@@ -0,0 +1,6 @@
+echo Hello World
+echo Thanks For Using upwn! The Better IPSW Decryption Utility,
+echo *Usage* to be ran as root 
+echo upwn_iPhone5,3_8.4.sh - For The iPhone 5C 5,3
+echo upwn_iphone5,4_8.4.sh - For The iPhone 5C 5,4
+echo upwn_log.sh - for changelog 
---
+++ SRC/usr/bin/upwn_log.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+echo upwn IPSW Download, Extraction, and Decryption Utility for the iPhone,
+
+echo *Change Log*
+echo Version 1.0 - initial Release
+echo Version 1.1 - Major Update To Provide Compatibility and support for upwn, Added ChangeLog, fixes several bugs that prevent binary from working properly,
+echo Version 1.2 - Updated Description, Added Source Code HTML Link To GitHub, Fixes An Issue That Causes Files To Reside After Uninstallation Hogging Space, upwn_log.log updated /usr/bin/upwn.sh updated, postrm added "patch to fix files residing", preinst updated, added upwnUpdateTut.txt
+echo Version 1.3 - Fixees a small issue in update
+echo Version 1.4 - Fixes all Known Bugs Finishing 100% support for iPhone 5,3 on iOS 8.4, updated changelog, added upwnlite.sh For Manual Extraction and decrypting, without having to download the IPSW
+echo Version 1.4.1 fixes installation error 
+echo Version 1.4.2 Adds Support For iPhone 5,4 for iOS 8.4 now and fixed a very minor issue in upwn_iPhone5,4_8.4.sh
+echo *Known Issues*
+echo For Now None, just make sure Devices is connected to wifi if you have limited data or want to ensure a faster download 
